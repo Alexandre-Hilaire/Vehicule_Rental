e911def2ce7142d2961500a94d1c0414dd0d9449
Monique-Silva <monique.silva-ximenes@le-campus-numerique.fr>
2023-09-07 16:06:03 +0200

    "Ajout d'un début du VehiculeController et ajout des dependencies sur pom.xml"

---
+++ src/main/java/com/example/Vehicule_Rental/controller/VehiculeController.java
@@ -0,0 +1,28 @@
+package com.example.Vehicule_Rental.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rent")
+public class VehiculeController {
+
+//@Autowired
+//private VehiculeRepository vehiculeRepository;
+}
+
+    /**Location de vehicules: Le client arrive dans un site, il y a un formulaire. il doit saisir:
+     * la data de debut de la location
+     * la date de fin de la location
+     * il y a la liste de vehicules disponibles qui s'affiche
+     * il seleccione la voiture desirée
+     * il est affiché les details de la voiture
+     * le client choisi le vehicule
+     * il doit remplir un formulaire pour renseigner ses infos
+     * si les infos sont valides, il peut passer à la reservation
+     * àl a reservation, il doit estimer les km
+     * le programme calcule et affiche le prix estimé et il valide la reservation
+     * à la date de la fin de la reservation, les vrais km sont relevé et le nouveau prix calculé
+     */
+

5219fc14b96fc07b1b97c4dad4f3f32449ac0ff4
Alexandre-Hilaire <alexandre.hilaire@le-campus-numerique.fr>
2023-09-07 14:08:59 +0200

    Ajout de la structure du projet

---
+++ src/main/java/com/example/Vehicule_Rental/VehiculeRentalApplication.java
@@ -0,0 +1,13 @@
+package com.example.Vehicule_Rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiculeRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiculeRentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Vehicule_Rental/VehiculeRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Vehicule_Rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiculeRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

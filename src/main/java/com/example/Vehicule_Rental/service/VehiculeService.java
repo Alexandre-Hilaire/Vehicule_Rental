f840b856c89bb056912089316a720c01a0d7c42b
Monique-Silva <monique.silva-ximenes@le-campus-numerique.fr>
2023-09-08 10:55:59 +0200

    Ajout de la classe VehiculeService

---
+++ src/main/java/com/example/Vehicule_Rental/service/VehiculeService.java
@@ -0,0 +1,15 @@
+package com.example.Vehicule_Rental.service;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.Vehicule_Rental.repository.
+@Data
+@Service
+public class VehiculeService {
+
+    @Autowired
+    private VehiculeRepository vehiculeRepository;
+
+    public
+}

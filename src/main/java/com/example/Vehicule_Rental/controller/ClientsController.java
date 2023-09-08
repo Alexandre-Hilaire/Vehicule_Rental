6c20a08418a526780be2955c842cb7c53f37ac29
Alexandre-Hilaire <alexandre.hilaire@le-campus-numerique.fr>
2023-09-08 14:07:11 +0200

    Ajout des clients/reservation/vehicule controllers

--- src/main/java/com/example/Vehicule_Rental/controller/ClientsController.java
+++ src/main/java/com/example/Vehicule_Rental/controller/ClientsController.java
@@ -4,6 +4,7 @@ import com.example.Vehicule_Rental.model.Clients;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.Optional;
 
 @RestController
@@ -11,7 +12,10 @@ import java.util.Optional;
 public class ClientsController {
     @Autowired
     private ClientService clientService;
-
+    @PostMapping
+    public Clients createClients(@RequestBody Clients clients){
+        return clientService.save_clients(clients);
+    }
     @GetMapping
     public Iterable<Clients>getClients(){
         return clientService.getClients();
@@ -21,20 +25,34 @@ public class ClientsController {
         Optional<Clients> client = clientService.getClient(id);
         return client.orElse(null);
     }
-    @PostMapping
-    public Clients createClients(@RequestBody Clients clients){
-        return clientService.save_clients(clients);
-    }
+
     @PutMapping("{id}")
-    public Clients updateClients(@PathVariable("id") final int id, @RequestBody Clients clients){
+    public Clients updateClients(@PathVariable("id") final int id, @RequestBody Clients clients) {
         Optional<Clients> client = clientService.getClient(id);
-        if (client.isPresent()){
+        if (client.isPresent()) {
             Clients currentClient = client.get();
             String last_name = clients.getLast_name();
-            if (last_name != null){
+            if (last_name != null) {
                 currentClient.setLast_name(last_name);
             }
-
+            String first_name = clients.getFirst_name();
+            if (first_name != null) {
+                currentClient.setFirst_name(first_name);
+            }
+            Date datebirth = clients.getDatebirth();
+            if (datebirth != null) {
+                currentClient.setDatebirth(datebirth);
+            }
+            int license_number = clients.getLicense_number();
+            currentClient.setLicense_number(license_number);
+            return currentClient;
+        } else {
+            return null;
         }
     }
+    @DeleteMapping
+    public void deleteClients(@PathVariable("id") final int id){
+        clientService.deleteClient(id);
+    }
+
 }
